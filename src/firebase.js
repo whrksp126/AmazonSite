f9db9cbff9aff81545ba63d58668a425ab48feb2
whrksp126 <whrksp126@naver.com>
2021-08-09 18:56:28 +0900

    first commit AmazonSite base setting, firebase setting

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyBXQGC7ulj4LZ4J3WSbVuNZXjZz3lZcQ88',
+  authDomain: 'gh-ddc43.firebaseapp.com',
+  projectId: 'gh-ddc43',
+  storageBucket: 'gh-ddc43.appspot.com',
+  messagingSenderId: '437329629079',
+  appId: '1:437329629079:web:df7fc525ceb39931c1fed2',
+  measurementId: 'G-3L9BNQS0WB',
+};
+
+// firebase 사이트에서 프로젝트 설정에 들어가서 sdk 설정을 구성을 변경하고 아래 내용을 복사한다
